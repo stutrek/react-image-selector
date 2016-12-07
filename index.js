502ac283c25e21e1775ac32334db6be9184236a2
Stu Kabakoff <stuart@compstak.com>
2016-12-07 13:04:31 -0500

    initial commit

---
+++ index.js
@@ -0,0 +1,40 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import { watchImage, addSource } from 'image-selector';
+
+const keys = [
+	'width',
+	'aspectRatio',
+	'src',
+	'at2x'
+];
+
+export class Image extends React.Component {
+
+	componentDidMount () {
+		var cuts = this.props.children.map(child => {
+			return keys.reduce((acc, prop) => {
+				if (child.props[prop] !== undefined) {
+					acc[prop] = child.props[prop];
+				}
+				return acc;
+			}, {});
+		});
+
+		var img = ReactDOM.findDOMNode(this);
+		if (this.props.responsive) {
+			watchImage(img, cuts);
+		} else {
+			addSource(img, 'src', cuts);
+		}
+	}
+
+	render () {
+		return (<img src={this.props.src} alt={this.props.alt} className={this.props.className || ''} />);
+	}
+}
+
+export class Cut extends React.Component {
+	render () {}
+}
